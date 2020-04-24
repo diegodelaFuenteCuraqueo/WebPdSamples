688391890e4d0f7c3dfbc977497b7c7079869335
diegodelaFuenteCuraqueo <diego.delafuente@ug.uchile.cl>
2020-04-24 01:33:55 -0400

    v0.0.1

---
+++ scripts/script1.js
@@ -0,0 +1,14 @@
+webPdExamples.init()
+
+$('form').submit(function(event) {
+  event.preventDefault()
+  Pd.send('metroTime', [parseFloat($('#metroTime').val())])
+})
+
+var patch
+$.get('pd/main.pd', function(mainStr) {
+  // Loading the patch
+  patch = Pd.loadPatch(mainStr)
+
+  webPdExamples.patchLoaded(mainStr)
+})
